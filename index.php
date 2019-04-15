4e7de7c7a88744c2a74648b5f24fe6f714ae88de
rhill34 <rhill34@mail.greenriver.edu>
2019-04-15 16:13:16 -0400

    Added Index for Controller

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Practice Routing
+ */
+// Turn on error reporting
+ini_set
+('display_errors', 1);
+error_reporting(E_ALL );
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function ()
+{
+    echo'FOOD!!!';
+//    //Display a view
+//    $view = new Template();
+//    echo $view->render('views/home2.html');
+
+}
+);
+//Run fat free
+$f3->run();
+
